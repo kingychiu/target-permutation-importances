d1b83f0586473a8cdee6c82bc64bda5194a05c7d
Anthony Chiu <kingychiu@gmail.com>
2023-07-02 14:49:58 +0800

    Fix lint and test coverage

--- tests/test_compute.py
+++ tests/test_compute.py
@@ -2,12 +2,16 @@ import pandas as pd
 from sklearn.datasets import load_breast_cancer
 from sklearn.ensemble import RandomForestClassifier
 
-from target_permutation_importances import compute
+from target_permutation_importances import (
+    compute,
+    compute_permutation_importance_by_division,
+    compute_permutation_importance_by_subtraction,
+)
 
 data = load_breast_cancer()
 
 
-def test_compute_with_sklearn():
+def test_compute_with_sklearn_by_subtraction():
     Xpd = pd.DataFrame(data.data, columns=data.feature_names)
     result_df = compute(
         model_cls=RandomForestClassifier,
@@ -15,6 +19,24 @@ def test_compute_with_sklearn():
             "n_estimators": 1,
         },
         model_fit_params={},
+        permutation_importance_calculator=compute_permutation_importance_by_subtraction,
+        X=Xpd,
+        y=data.target,
+        num_actual_runs=2,
+        num_random_runs=10,
+    )
+    assert isinstance(result_df, pd.DataFrame)
+
+
+def test_compute_with_sklearn_by_division():
+    Xpd = pd.DataFrame(data.data, columns=data.feature_names)
+    result_df = compute(
+        model_cls=RandomForestClassifier,
+        model_cls_params={
+            "n_estimators": 1,
+        },
+        model_fit_params={},
+        permutation_importance_calculator=compute_permutation_importance_by_division,
         X=Xpd,
         y=data.target,
         num_actual_runs=2,
