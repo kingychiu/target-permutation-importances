511257cb1eaf5e606483720fb9b75db6c60ca5ba
Anthony Chiu <kingychiu@gmail.com>
2023-07-06 14:42:41 +0800

    Update Benchmarks  (#7)

--- benchmarks/process_result_csv.py
+++ benchmarks/process_result_csv.py
@@ -1,20 +1,24 @@
 import pandas as pd
 
-df = pd.read_csv("./benchmarks/results/random_forest_tabular_benchmark.csv")
+df = pd.read_csv("./benchmarks/results/tabular_benchmark.csv")
 df["feature_reduction"] = (
     df["num_features"].astype(str) + "->" + df["selected_num"].astype(str)
 )
 df["% Change from baseline"] = df["% Change from baseline"].astype(str) + "%"
-for col in ["importances", "test_score", "% Change from baseline"]:
+for col in ["importances", "test_score", "% Change from baseline", "feature_reduction"]:
     df.loc[df["is_best"], col] = df.loc[df["is_best"], col].apply(lambda x: f"**{x}**")
 
-df[
-    [
-        "dataset",
-        "importances",
-        "feature_reduction",
-        "test_score",
-    ]
-].to_csv(
-    "./benchmarks/results/random_forest_tabular_benchmark_processed.csv", index=False
-)
+models = df["model"].unique()
+
+for model in models:
+    df[df["model"] == model][
+        [
+            "dataset",
+            "importances",
+            "feature_reduction",
+            "test_score",
+        ]
+    ].to_csv(
+        f"./benchmarks/results/processed_{model}_tabular_benchmark.csv",
+        index=False,
+    )
